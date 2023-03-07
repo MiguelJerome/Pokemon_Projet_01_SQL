8673a57db74b2b97a6b946e8bc4ed10ffefec3a9
MiguelJerome <sharkmaycry@gmail.com>
2023-03-07 05:06:57 -0500

    fix database creation

--- Pokemon_Creation_BD_etTables_V2.sql
+++ Pokemon_Creation_BD_etTables_V2.sql
@@ -5,14 +5,12 @@ END
 
 USE Pokemon2;
 
-
-
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Regions' AND type = 'U')
 BEGIN
     CREATE TABLE Regions(
         Nom_region NVARCHAR(8) NOT NULL PRIMARY KEY,
         Nom_ville NVARCHAR(30) NOT NULL,
-		CONSTRAINT UC_Number UNIQUE (Nom_region)
+		CONSTRAINT UC_Regions UNIQUE (Nom_region)
     );
 END
 
@@ -21,10 +19,13 @@ BEGIN
     CREATE TABLE Generations(
         Gen_number INT NOT NULL PRIMARY KEY,
         Nom_region NVARCHAR(8) NOT NULL,
-		CONSTRAINT UC_Number UNIQUE (Gen_number),
+		CONSTRAINT UC_Generations UNIQUE (Gen_number),
         FOREIGN KEY(Nom_region) REFERENCES Regions
     );
 END
+
+
+
 
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Pokemon' AND type = 'U')
 BEGIN
@@ -40,21 +41,20 @@ BEGIN
         Sp_Attack INT NOT NULL,
         Sp_Defense INT NOT NULL,
         Speed INT NOT NULL,
-		CONSTRAINT UC_Number UNIQUE (Pokemon_number),
+		CONSTRAINT UC_Pokemon UNIQUE (Pokemon_number),
         FOREIGN KEY(Gen_number) REFERENCES Generations
     );
 END
 
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Type' AND type = 'U')
 BEGIN
-    CREATE TABLE Type(
-Type_id INT IDENTITY(1,1) PRIMARY KEY,
-Type_1 NVARCHAR(8) NOT NULL,
-Type_2 NVARCHAR(8),
-CONSTRAINT UC_Type UNIQUE (Type_1, Type_2),
-CONSTRAINT FK_Pokemon_Type_1 FOREIGN KEY (Type_1) REFERENCES Type (Type_1),
-CONSTRAINT FK_Pokemon_Type_2 FOREIGN KEY (Type_2) REFERENCES Type (Type_2)
-);
+	CREATE TABLE Type(
+		Pokemon_number INT NOT NULL,
+		Type_1 NVARCHAR(8) NOT NULL,
+		Type_2 NVARCHAR(8),
+        CONSTRAINT PK_Pokemon_Type PRIMARY KEY (Pokemon_number),
+        FOREIGN KEY(Pokemon_number) REFERENCES Pokemon(Pokemon_number)
+	);
 END
 
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Pokedex' AND type = 'U')
@@ -64,7 +64,7 @@ BEGIN
         Gen_number INT NOT NULL,
         Nom_Pokemon NVARCHAR(255) NOT NULL,
         Pokemon_number INT NOT NULL,
-		CONSTRAINT UC_Number UNIQUE (Nom_du_jeu),
+		CONSTRAINT UC_Pokedex UNIQUE (Nom_du_jeu),
         FOREIGN KEY(Gen_number) REFERENCES Generations,
         FOREIGN KEY(Pokemon_number) REFERENCES Pokemon
     );
@@ -77,7 +77,7 @@ BEGIN
         Team_type NVARCHAR(15) NOT NULL,
         Nom_du_jeu NVARCHAR(255) NOT NULL,
         Nom_region NVARCHAR(8) NOT NULL,
-		CONSTRAINT UC_Number UNIQUE (Nom),
+		CONSTRAINT UC_Champions UNIQUE (Nom),
         FOREIGN KEY(Nom_region) REFERENCES Regions,
         FOREIGN KEY(Nom_du_jeu) REFERENCES Pokedex
     );
@@ -90,7 +90,7 @@ BEGIN
         Gen_number INT NOT NULL,
         Gender NVARCHAR(15) NOT NULL,
         Team_size TINYINT NOT NULL, /*Max 6*/
-		CONSTRAINT UC_Number UNIQUE (Nom),
+		CONSTRAINT UC_Dresseur UNIQUE (Nom),
         FOREIGN KEY(Nom) REFERENCES Champions,
         FOREIGN KEY(Gen_number) REFERENCES Generations
     );
@@ -99,37 +99,37 @@ END
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Pokemon_Dresseur' AND type = 'U')
 BEGIN
     CREATE TABLE Pokemon_Dresseur(
-Nom_dresseur NVARCHAR(15) NOT NULL,
-Pokemon_number INT NOT NULL,
-Pokemon_level TINYINT,
-CONSTRAINT UC_Number UNIQUE (Nom_dresseur),
-FOREIGN KEY(Nom_dresseur) REFERENCES Dresseur,
-FOREIGN KEY(Pokemon_number) REFERENCES Pokemon
-);
+		Nom_dresseur NVARCHAR(15) NOT NULL,
+		Pokemon_number INT NOT NULL,
+		Pokemon_level TINYINT,
+		CONSTRAINT UC_Pokemon_Dresseur UNIQUE (Nom_dresseur),
+		FOREIGN KEY(Nom_dresseur) REFERENCES Dresseur,
+		FOREIGN KEY(Pokemon_number) REFERENCES Pokemon
+	);
 END
 
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Capacite' AND type = 'U')
 BEGIN
-CREATE TABLE Capacite(
-Nom NVARCHAR(15) NOT NULL PRIMARY KEY,
-Lev_unlock INT NOT NULL,
-Power_cap INT,
-Accuracy INT,
-PP INT NOT NULL,
-CONSTRAINT UC_Number UNIQUE (Nom),
-Category NVARCHAR(12) NOT NULL,
-Type_cap NVARCHAR(8) NOT NULL
-);
+	CREATE TABLE Capacite(
+		Nom NVARCHAR(15) NOT NULL PRIMARY KEY,
+		Lev_unlock INT NOT NULL,
+		Power_cap INT,
+		Accuracy INT,
+		PP INT NOT NULL,
+		CONSTRAINT UC_Capacite UNIQUE (Nom),
+		Category NVARCHAR(12) NOT NULL,
+		Type_cap NVARCHAR(8) NOT NULL
+	);
 END
 
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Pokemon_Capacite' AND type = 'U')
 BEGIN
-CREATE TABLE Pokemon_Capacite(
-Pokemon_number INT NOT NULL,
-Nom NVARCHAR(15) NOT NULL,
-Level INT NOT NULL,
-CONSTRAINT UC_Number UNIQUE (Pokemon_number),
-FOREIGN KEY(Pokemon_number) REFERENCES Pokemon,
-FOREIGN KEY(Nom) REFERENCES Capacite
-);
+	CREATE TABLE Pokemon_Capacite(
+		Pokemon_number INT NOT NULL,
+		Nom NVARCHAR(15) NOT NULL,
+		Level INT NOT NULL,
+		CONSTRAINT UC_Pokemon_Capacite UNIQUE (Pokemon_number, Nom),
+		FOREIGN KEY(Pokemon_number) REFERENCES Pokemon,
+		FOREIGN KEY(Nom) REFERENCES Capacite
+	);
 END
